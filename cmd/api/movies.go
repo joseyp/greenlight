194c7d041fc383cf987986cf0fa806976f39409e
Jose Yolanda Purba <josprb@gmail.com>
2023-10-24 16:18:58 +0700

    feat: added get movie route

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"greenlight.joseyp.dev/internal/data"
 	"greenlight.joseyp.dev/internal/validator"
@@ -59,13 +59,15 @@ func (app *application) showMovieHandler(w http.ResponseWriter, req *http.Reques
 		return
 	}
 
-	movie := data.Movie{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "Casablanca",
-		Runtime:   102,
-		Genres:    []string{"drama", "romance", "war"},
-		Version:   1,
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, req)
+		default:
+			app.serverErrorResponse(w, req, err)
+		}
+		return
 	}
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
